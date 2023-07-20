13c0d048e4e015175f3fc5a64770f952188db1c8
lesponns <lesponns@DESKTOP-44MF2L6>
2023-07-20 19:03:58 +0300

    Proje dosyası ekle.

---
+++ SmartPro_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Değişken kayıt
+            double Weight, Height, bmi;
+
+            //Kullanıcıdan Kilo değeri alma
+            Console.WriteLine("What is your weight?");
+            Console.Write("Weight: ");
+            Weight= double.Parse(Console.ReadLine());
+            Console.Clear();
+
+            //Kullanıcıdan boy değeri alma
+            Console.WriteLine("What is your height?");
+            Console.Write("Height: ");
+            Height = double.Parse(Console.ReadLine());
+            Console.Clear();
+
+            //VKI hesaplama vki = kg / metre*metre
+            bmi = Weight / (Height * Height);
+
+
+            
+            Console.WriteLine("Your body mass index: " + bmi);
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
