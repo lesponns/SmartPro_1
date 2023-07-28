c1f1d48d622b36af207ccc8bb826b5139f48ac7f
lesponns <71814927+lesponns@users.noreply.github.com>
2023-07-28 06:57:09 +0300

    Update Program.cs

--- SmartPro_1/Program.cs
+++ SmartPro_1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,16 +10,16 @@ namespace SmartPro_1
     {
         static void Main(string[] args)
         {
-            //Değişken kayıt
+            //Değişkenler
             double Weight, Height, bmi;
 
-            //Kullanıcıdan Kilo değeri alma
+            //Kullanıcının Ağırlık Bilgisi
             Console.WriteLine("What is your weight?");
             Console.Write("Weight: ");
             Weight= double.Parse(Console.ReadLine());
             Console.Clear();
 
-            //Kullanıcıdan boy değeri alma
+            //Kullanıcının Uzunluk Bilgisi
             Console.WriteLine("What is your height?");
             Console.Write("Height: ");
             Height = double.Parse(Console.ReadLine());
@@ -28,20 +28,7 @@ namespace SmartPro_1
             //VKI hesaplama vki = kg / metre*metre
             bmi = Weight / (Height * Height);
 
-
-            
             Console.WriteLine("Your body mass index: " + bmi);
-
-
-
-
-
-
-
-
-
-
-
             Console.ReadKey();
         }
     }
